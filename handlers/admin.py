b84995f68965e371de101b7c57decf7358d45c6e
Oleksandr Sosluiev <58919595+asosluev@users.noreply.github.com>
2025-11-09 18:23:07 +0200

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,12 +1,12 @@
 # handlers/admin.py
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
-from config import ADMIN_IDS
+from config import TG_ADMINS
 from handlers.menu import menu_manager
 
 async def reload_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
-    if user_id not in ADMIN_IDS:
+    if user_id not in TG_ADMINS:
         await update.message.reply_text("⛔ Лише для адміністратора.")
         return
     try:
